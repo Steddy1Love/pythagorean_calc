aea0771774e6c1513a1b9ce62975c0e583b62b84
Steddman Bell <steddfast@gmail.com>
2024-08-23 17:43:47 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+side_a = int(input("Input side a"))
+side_b = int(input("Input side b"))
+print(f"The width of the triangle is: {side_a}")
+print(f"The height of the triangle is: {side_b}")
+
+side_c = (side_a ** 2 + pow(side_b, 2)) ** (1/2)
+
+print(f"C equals: {side_c}")
